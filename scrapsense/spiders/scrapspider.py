7328f930ff352612bd245a51c96555f490aafb63
Annlee Fores <annleefores@gmail.com>
2022-08-08 15:35:08 +0530

    spider for bms removed

--- scrapsense/spiders/scrapspider.py
+++ scrapsense/spiders/scrapspider.py
@@ -3,14 +3,13 @@ from dateutil import parser
 import re
 
 
-# playwright integration for js website  Note: Make sure to edit settings.py to include playwright
 
 # remove all symbols from string and join together
 def compareRegex(movie):
     movie = re.sub(r'[^\w]', '', movie)
     return movie
 
-# spider for ticketnew website
+# spider for ticketnew websited
 class tkSpider(scrapy.Spider):
     name = 'tk'
     
@@ -45,45 +44,6 @@ class tkSpider(scrapy.Spider):
             yield {
                 None
             }
-
-# spider for bookmyshow website
-class bmsSpider(scrapy.Spider):
-    name = 'bms'
-
-    def start_requests(self):
-        yield scrapy.Request(
-            url=f'https://in.bookmyshow.com/buytickets/{self.link}',
-            # PageMethod('wait_for_selector', 'ul#showEvents')
-           )
-
-    async def parse(self, response):
-
-        film = self.film
-        date = self.date
-
-        ## if date booking is active get the details
-        active = response.css('li.slick-current').get()
-
-        if active != None:
-            venuehtml =  response.css('a.venue-heading')
-
-            date_numeric = response.css('li.date-details.slick-slide.slick-current.slick-active > a > div.date-numeric::text').get()
-            date_month = response.css('li.date-details.slick-slide.slick-current.slick-active > a > div.date-month::text').get()
-
-            DT = (parser.parse(f'{date_month} {date_numeric} 2022')).strftime('%Y-%m-%d')
-            if DT == date:
-                for show in response.css('a.nameSpan'):
-                    showname = show.css('a::text').get()
-                    if compareRegex(film) in compareRegex(showname.lower()):
-                        yield {
-                            'venue' : venuehtml.css('a::text').get(),
-                            'show' : showname,
-                            'date' : DT,
-                        }
-            else:
-                yield {
-                    None
-                }
 
 
 # spider for sourcing ticketnew booking links and theater lists
