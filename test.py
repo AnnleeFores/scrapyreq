777cfc347846f19354c6526a3796835ac8869bb5
Annlee Fores <annleefores@gmail.com>
2022-08-08 15:42:04 +0530

    test files removed

--- test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-ts = 1660089600000 / 1000
-
-# if you encounter a "year is out of range" error the timestamp
-# may be in milliseconds, try `ts /= 1000` in that case
-print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d'))
-print(datetime.utcfromtimestamp(1659897000).strftime('%Y-%m-%d'))
